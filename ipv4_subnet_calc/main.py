1eaafb59db2a7c39c814ddd8f687f6a25d05d24b
Kyoto-01 <62781857+Kyoto-01@users.noreply.github.com>
2021-07-11 18:12:36 -0300

    refatorando ipv4_subnet_calc

--- ipv4_subnet_calc/main.py
+++ ipv4_subnet_calc/main.py
@@ -1,19 +1,19 @@
-def find_net_addr(ip, subnet_len):
-    net_octs = subnet_len // 8
-    qtd_net_octs = net_octs
-    net_addr = [ip[i] for i in range(net_octs)]
-    net_octs = subnet_len % 8
+# Função p/ encontrar o endereço de rede
+def find_net_address(ip_octects: list[str], subnet_len: int) -> str:
+    qtd_full_net_octects = subnet_len // 8
+    qtd_borrowed_bits = subnet_len % 8
+    net_address = [ip_octects[i] for i in range(qtd_full_net_octects)]
 
-    if net_octs > 0:
-        last_oct = 255 - (2**(8 - net_octs)) + 1
+    if qtd_full_net_octects > 0:
+        last_oct = 255 - (2**(8 - qtd_full_net_octects)) + 1
         last_oct = str(last_oct)
-        net_addr.append(last_oct)
-        qtd_net_octs += 1
+        net_address.append(last_oct)
+        qtd_full_net_octects += 1
 
-    for i in range(qtd_net_octs, len(ip)):
-        net_addr.append('0')
+    for i in range(qtd_full_net_octects, len(ip_octects)):
+        net_address.append('0')
 
-    print('.'.join(net_addr))
+    return '.'.join(net_address)
 
 
 ip_addr, net_length = input('Seu IPv4 e prefixo CIDR de rede: ').split('/')
@@ -22,7 +22,4 @@ net_length = int(net_length)
 subnet_length = input('Sua máscara de subrede em CIDR: ').replace('/', '')
 subnet_length = int(subnet_length)
 
-
-print(ip_addr, net_length, subnet_length)
-
-find_net_addr(ip_addr, subnet_length)
+print(find_net_address(ip_addr, subnet_length))
