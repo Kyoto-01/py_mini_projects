21d67014f2d90c4a8c4e1efdb1f57d15152be9bd
kyoto-01 <cameraman269@gmail.com>
2021-09-15 11:20:25 -0300

    resolve all TODOS and add more
    * added methods to find min and max nodes in BinaryTree.
    * added method to find the height of BinaryTree (TODO to debug)

--- data_structures/hierarchical/trees.py
+++ data_structures/hierarchical/trees.py
@@ -14,11 +14,27 @@ ROOT = 'root'
 
 class BinaryTree:
     
-    # TODO add method to count nodes in tree
-    
     def __init__(self, root: 'Node' = None):
         self.root = root
-        
+
+    def count(self, root: 'Node' = ROOT):
+        if root == ROOT:
+            root = self.root
+        if root is None:
+            return 0
+
+        return 1 + self.count(root.left_child) + self.count(root.right_child)
+
+    def height(self, node: 'Node' = ROOT):
+        if node == ROOT:
+            node = self.root
+        if node is None:
+            return 0
+        left_height = 1 + self.height(node.left_child)
+        right_height = 1 + self.height(node.right_child)
+
+        return left_height if left_height > right_height else right_height
+
     def pre_order_nav(self, root: 'Node' = ROOT):
         if root == ROOT:
             root = self.root
@@ -60,10 +76,26 @@ class BinaryTree:
             
 class BinarySearchTree(BinaryTree):
     
-    # TODO add methods to find the min and the max childs 
-    
     def __init__(self, root: 'Node' = None):
         super().__init__(root)
+
+    def min_child(self, root: 'Node' = ROOT):
+        if root == ROOT:
+            root = self.root
+        if root:
+            while root.left_child:
+                root = root.left_child
+
+        return root
+
+    def max_child(self, root: 'Node' = ROOT):
+        if root == ROOT:
+            root = self.root
+        if root:
+            while root.right_child:
+                root = root.right_child
+
+        return root
         
     def insert(self, data: object, root: 'Node' = ROOT):
         if root == ROOT:
@@ -75,29 +107,29 @@ class BinarySearchTree(BinaryTree):
                 root.left_child = self.insert(data, root.left_child)
             if data > root.data:
                 root.right_child = self.insert(data, root.right_child)
-                
+
         return root 
     
     def remove(self, key: object, root: 'Node' = ROOT):
         if root == ROOT:
-            root = self.root
-        if root is None:
-            return root
-        if key < root.data:
-            root.left_child = self.remove(key, root.left_child)
-        elif key > root.data:
-            root.right_child = self.remove(key, root.right_child)
+            self.root = self.remove(key, self.root)
         else:
-            if not root.left_child:
-                return root.right_child
-            if not root.right_child:
-                return root.left_child
+            if root is None:
+                return None
+            if key < root.data:
+                root.left_child = self.remove(key, root.left_child)
+            elif key > root.data:
+                root.right_child = self.remove(key, root.right_child)
+            else:
+                if not root.left_child:
+                    return root.right_child
+                if not root.right_child:
+                    return root.left_child
 
-            actual = root.right_child
-            while actual.left_child:
-                actual = actual.left_child
-            root.data = actual.data
-            root.right_child = self.remove(actual.data, root.right_child)
+                min_right_child = self.min_child(root.right_child)
+                root.data = min_right_child.data
+                root.right_child = self.remove(min_right_child.data, root.right_child)
+
         return root
 
     def search(self, key: object, root: 'Node' = ROOT):
@@ -109,16 +141,19 @@ class BinarySearchTree(BinaryTree):
             return self.search(key, root.left_child)
         if key > root.data:
             return self.search(key, root.right_child)
+
         return root
-        
         
         
 if __name__ == '__main__':
     
     #         6
+    #        /
     #      2
-    #   1     4
-    # 0      3  5
+    #     / \
+    #   1    4
+    #  /    / \
+    # 0    3   5
         
     nums = BinarySearchTree()
     nums.insert(6)
@@ -130,27 +165,47 @@ if __name__ == '__main__':
     nums.insert(5)
 
     nums.in_order_nav()
+    print(f'[{nums.count()} node(s)]')
+    print(f'[min = {nums.min_child()}; max = {nums.max_child()}]')
+    print(f'[height = {nums.height()}]')
     print()
 
     # remove tests
-    nums.remove(5)
+    nums.remove(6)
     nums.in_order_nav()
+    print(f'[{nums.count()} node(s)]')
+    print(f'[min = {nums.min_child()}; max = {nums.max_child()}]')
+    print(f'[height = {nums.height()}]')
     print()
 
     nums.remove(1)
     nums.in_order_nav()
+    print(f'[{nums.count()} node(s)]')
+    print(f'[min = {nums.min_child()}; max = {nums.max_child()}]')
+    print(f'[height = {nums.height()}]')
     print()
-    
-    nums.remove(3)
+
+    nums.remove(2)
     nums.in_order_nav()
+    print(f'[{nums.count()} node(s)]')
+    print(f'[min = {nums.min_child()}; max = {nums.max_child()}]')
+    print(f'[height = {nums.height()}]')
     print()
-    
+
+    nums.remove(4)
+    nums.in_order_nav()
+    print(f'[{nums.count()} node(s)]')
+    print(f'[min = {nums.min_child()}; max = {nums.max_child()}]')
+    print(f'[height = {nums.height()}]')
+    print()
+
     # search tests
+    #   find
     print(nums.search(0))
     print(nums.search(2))
     print(nums.search(4))
     print(nums.search(6))
-    
+    #   not find
     print(nums.search(5))
     print(nums.search(1))
     print(nums.search(3))
